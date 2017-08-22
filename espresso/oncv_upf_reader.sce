7e3812bc3b5ac04bef6833066e46b33ec03a7f3b
pipidog <pipidog@gmail.com>
2017-08-22 16:00:49 -0700

    hiq_read.py update

--- espresso/oncv_upf_reader.sce
+++ espresso/oncv_upf_reader.sce
@@ -3,23 +3,25 @@
 clear; clc; exec(PiLib);
 // Parameter ===========================================================
 work_dir=[]//'C:\MyDrive\Work\ONCV\Te'
-input_name='Pt_ONCV_PBE_fr.upf'
-output_name='Pt_ONCV_PBE_fr.upf'
+input_name='Cu_ONCV_PBE_fr.upf'
+output_name='Cu_ONCV_PBE_fr.upf'
 pp_type='upf'   // 'psp8' / 'upf'
 // Main ================================================================
 work_dir=PIL_dir_path(work_dir);
 fid=mopen(work_dir+input_name,'r');
 intext=mgetl(fid,-1)
+mclose(fid)
 select pp_type
 case 'upf'
-    [r,w]=grep(intext,'Begin PSP_UPF');
-    mclose(fid)
+    row=grep(intext,'Begin PSP_UPF');
 case 'psp8'
-    [r,w]=grep(intext,'Begin PSPCODE8');
-    mclose(fid)
+    row=grep(intext,'Begin PSPCODE8');
+end
+if row==[] then
+    disp('Error: no PSP begin keyword found')
+    abort
 end
 
-
 fid=mopen(work_dir+'\'+output_name,'w');
-mputl(intext(r+1:$),fid)
+mputl(intext(row+1:$),fid)
 mclose(fid)
--- lmtart/hiq_read.py
+++ lmtart/hiq_read.py
@@ -1,6 +1,8 @@
 # This code is to read the hiq data output by LMTART 8.0
 from functools import reduce
+import os, sys
 import numpy as np
+import matplotlib.pyplot as plt
 class hiqpp:
     def __init__(self,wkdir,prefix):
         if (wkdir[-1]!='/') & (wkdir[-1]!='\\'):
@@ -8,7 +10,7 @@ class hiqpp:
         self.wkdir=wkdir
         self.prefix=prefix
         
-    def read(self,nqdiv,nOmgCHI):
+    def hiqread(self,nqdiv,nOmgCHI):
         nqdiv=np.array(nqdiv)+1
         with open(self.wkdir+prefix+'.hiq') as file:
             flines=file.readlines()
@@ -16,54 +18,102 @@ class hiqpp:
         lr=[n for n, line in enumerate(flines) if line.find('<SECTION=REHI>')!=-1]
         li=[n for n, line in enumerate(flines) if line.find('<SECTION=IMHI>')!=-1]
         
-        print(lr,li)
-        freq_n=0
-        freq=np.zeros((nOmgCHI+1))
-        hi_r=np.zeros((reduce(lambda x,y:x*y,nqdiv),4,nOmgCHI+1))
-        hi_i=np.zeros((reduce(lambda x,y:x*y,nqdiv),4,nOmgCHI+1))
-        kpoint=np.zeros((reduce(lambda x,y:x*y,nqdiv),3))
-        
-        for part_n, part in enumerate([[lr[0],li[0]],[li[0],len(flines)]]):
-            freq_n=0
-            print(part)
-            for n, line in enumerate(flines[part[0]:part[1]]):
-                if line.find('! Frequency'):
-                    line=line.replace('! Frequency','!Frequency')
-                
-                line_dat=line.split()
-                if (len(line_dat)==2):  #freq
-                    freq_n+=1
-                    freq[freq_n-1]=float(line_dat[0])
-                    k_n=0
-                    print(line[0:-1]+' --> {0}\n'.format(n+10))
-                    
-                if (len(line_dat)==3):  # kpoint 
-                    k_n+=1
-                    spin_n=0
-                    print([float(line_dat_n) for line_dat_n in line_dat])
-                    kpoint[k_n-1,:]=[float(line_dat_n) for line_dat_n in line_dat]
-                    print(line[0:-1]+' --> {0}\n'.format(n+10))
-                    
-                if (len(line_dat)==1):
-                    spin_n+=1
-                    if part_n==0:
-                        hi_r[k_n-1,spin_n-1,freq_n-1]=float(line_dat[0])
-                    elif part_n==1:
-                        hi_i[k_n-1,spin_n-1,freq_n-1]=float(line_dat[0])
-                    print(line[0:-1]+' --> {0}\n'.format(n+10))  
+        # determin # of bands
+        tot_freq=(nOmgCHI+1)
+        tot_k=reduce(lambda x,y:x*y,nqdiv)
+        tot_line=li[0]-lr[0]-1
+        tot_ban_line=int((tot_line-(tot_freq)-(tot_k*tot_freq))/(tot_k*tot_freq*4))
+        tot_ban=int(np.floor(np.sqrt(tot_ban_line*5-1)))
 
-        np.savez(self.wkdir+'hiq_data.npz',hi_r=hi_r,hi_i=hi_i,freq=freq,kpoint=kpoint)
-        #print(tot_freq,tot_k,tot_sum)
-        # print(nVal.count(1))
-        # print(nVal.count(2))
-        # print(nVal.count(3))    
+        # double check band
+        if np.ceil((tot_ban**2+1)/5)*tot_freq*tot_k*4+tot_k*tot_freq+tot_freq != tot_line:
+            print('Error: tot_ban not detected correctly!')        
+            sys.exit()
+            
+        f_n=0
+        freq=np.zeros((tot_freq))
+        hi_mat=np.zeros((tot_ban,tot_ban,4,tot_k,tot_freq))*1j
+        hi_sum=np.zeros((tot_k,4,tot_freq))*1j
+        kpoint=np.zeros((tot_k,3))
+        
+        lcount=9
+        for part in ['r','i']:
+            print(flines[lcount])
+            lcount+=1
+            for f_n in range(0,tot_freq):
+                if flines[lcount].find('Frequency')==-1:
+                    print('Error: frequency read error!')
+                    sys.exit()
+                else:
+                    print('f_n={0}'.format(f_n))
+                    freq[f_n]=flines[lcount].split()[0]
+                    lcount+=1
+                    
+                for k_n in range(0,tot_k):
+                    if len(flines[lcount].split())!=3:
+                        print('Error: kpoint read error!')
+                        sys.exit()
+                    else:
+                        kpoint[k_n,:]=[float(ki) for ki in flines[lcount].split()]
+                        lcount+=1
+                    
+                    # scattering matrix area ----------------
+                    for spin_n in range(0,4):  # run spin
+                        mat_ele=[]
+                        for ban_line in range(0,tot_ban_line): # run scattering matrix
+                           mat_ele.extend([float(banval) for banval \
+                           in flines[lcount].split()])
+                           lcount+=1
+                           
+                        if len(mat_ele)==tot_ban**2+1:
+                            if part=='r':
+                                hi_sum[k_n,spin_n,f_n]+=mat_ele[-1]
+                                mat_ele=np.array(mat_ele[0:-1])
+                                mat_ele.shape=(tot_ban,tot_ban)
+                                hi_mat[:,:,spin_n,k_n,f_n]+=mat_ele
+                            elif part=='i':
+                                hi_sum[k_n,spin_n,f_n]+=mat_ele[-1]*1j
+                                mat_ele=np.array(mat_ele[0:-1])
+                                mat_ele.shape=(tot_ban,tot_ban)
+                                hi_mat[:,:,spin_n,k_n,f_n]+=mat_ele*1j
+                        else:
+                            print('Error: # of scattering matrix elements incorrect!')
+                            sys.exit()
+                    # scattering matrix area ----------------
+        
+        np.savez(self.wkdir+'hiq_data.npz',hi_mat=hi_mat,hi_sum=hi_sum,\
+        freq=freq,kpoint=kpoint)
+        
+    def hiqplot(self,spin,show):
+        hiq=np.load(self.wkdir+'hiq_data.npz')
+        if spin=='a':
+            for spin_n in range(0,4):
+                plt.subplot(2,2,spin_n+1)
+                plt.plot(hiq['hi_sum'][:,spin_n,0].imag)            
+        else:
+            plt.plot(hiq['hi_sum'][:,spin,0].imag)
+        
+        plt.savefig(self.wkdir+prefix+'-hiq.png')
+        if show=='on':
+            plt.show()
+        elif show=='off':
+            plt.close()
+        
 # main ===========================
 if __name__=='__main__':
     #parameter --------------
-    wkdir='C:\\MyDrive\\Work\\Eu\\lmtart\\pnma-afm\\U5-LDA-801\\Eu-100'
-    prefix='Eu-100'
-    nqdiv=[6,6,6] # in pnt file
-    nOmgCHI=1  # in hiq file
-    # run -------------------
-    hiq=hiqpp(wkdir,prefix)
-    hiq.read(nqdiv,nOmgCHI)
+    lat_range=[80,120,2]
+    for lat in range(lat_range[0],lat_range[1]+1,lat_range[2]):
+        print('lat={0}'.format(lat))
+        wkdir='/home/pipidog/work/lmtart/Eu/pnma-afm/U5-LDA-801/Eu-'+str(lat)
+        prefix='Eu-'+str(lat)
+        nqdiv=[6,6,6] # in pnt file
+        nOmgCHI=1  # in hiq file
+        spin=1
+        show='off'
+        # run -------------------
+        hiq=hiqpp(wkdir,prefix)
+        hiq.hiqread(nqdiv,nOmgCHI)
+        hiq.hiqplot(spin,show)
+
+
